f5420520466c0118d3ac08814eceea96bd09a5e4
David L. Inman <davidlevere@gmail.com>
2021-07-05 00:58:36 -0400

    create input module

--- src/lib.rs
+++ src/lib.rs
@@ -1,40 +1,9 @@
 
-use std::str::CharIndices;
 use std::collections::HashMap;
-use std::collections::VecDeque;
 
-struct Input<'a> {
-    cs : CharIndices<'a>,
-}
+mod input;
 
-impl<'a> Input<'a> {
-
-    fn new(s : &'a str) -> Input<'a> {
-        Input { cs : s.char_indices() }
-    }
-
-    fn get_char(&mut self) -> Result<char, ()> {
-        match self.cs.next() {
-            Some((_,c)) => Ok(c),
-            None => Err(()),
-        }
-    }
-
-    fn match_string(&mut self, s : &str) -> Result<(), ()> {
-        let mut n = self.cs.clone();
-
-        for c in s.chars() {
-            match n.next() {
-                Some((_, target)) if c == target => { }, 
-                Some(_) => return Err(()),
-                None => return Err(()),
-            }
-        }
-
-        self.cs = n;
-        Ok(())
-    }
-}
+use input::Input;
 
 enum ParseRule {
     Any,                                                            // Char(char) 
@@ -69,58 +38,4 @@ fn parse(rule : &str, rules : HashMap<String, ParseRule>, input : &mut Input) ->
 
 fn main() {
     println!("Hello, world!");
-}
-
-#[cfg(test)]
-mod test {
-    use super::*;
-
-    #[test]
-    fn input_get_char() {
-        let mut input = Input::new("string");
-
-        let c = input.get_char().expect("Should be able to get 's'");
-        assert_eq!( 's', c );
-
-        let c = input.get_char().expect("Should be able to get 't'");
-        assert_eq!( 't', c );
-
-        let c = input.get_char().expect("Should be able to get 'r'");
-        assert_eq!( 'r', c );
-
-        let c = input.get_char().expect("Should be able to get 'i'");
-        assert_eq!( 'i', c );
-
-        let c = input.get_char().expect("Should be able to get 'n'");
-        assert_eq!( 'n', c );
-
-        let c = input.get_char().expect("Should be able to get 'g'");
-        assert_eq!( 'g', c );
-    }
-
-    #[test]
-    fn match_string_failure_should_not_change_index() {
-        let mut input = Input::new("string");
-
-        let result = input.match_string("yy");
-
-        assert!( matches!( result, Err(_) ) );
-
-        let result = input.match_string("string");
-
-        assert!( matches!( result, Ok(_) ) );
-    }
-
-    #[test]
-    fn match_string_success_should_change_index() {
-        let mut input = Input::new("string");
-
-        let result = input.match_string("st");
-
-        assert!( matches!( result, Ok(_) ) );
-
-        let result = input.match_string("ring");
-
-        assert!( matches!( result, Ok(_) ) );
-    }
 }
